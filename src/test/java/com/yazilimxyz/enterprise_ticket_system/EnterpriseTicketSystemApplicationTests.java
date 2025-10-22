69ad10e240b8b0ed65f92d39f75a5cd5d63f571a
Türker Kıvılcım <kivturker@gmail.com>
2025-10-22 20:22:03 +0300

    feat: initialize java 21 project

---
+++ src/test/java/com/yazilimxyz/enterprise_ticket_system/EnterpriseTicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yazilimxyz.enterprise_ticket_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseTicketSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
