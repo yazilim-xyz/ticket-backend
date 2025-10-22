e501d23e18514a4b63a21680b3a963fd82bac88f
Vedat Ali Tatlı <vedat-mac@Vedat-MacBook-Air.local>
2025-10-23 00:31:23 +0300

    Databasesiz Çalışması için değiştirildi

--- src/test/java/com/yazilimxyz/enterprise_ticket_system/EnterpriseTicketSystemApplicationTests.java
+++ src/test/java/com/yazilimxyz/enterprise_ticket_system/EnterpriseTicketSystemApplicationTests.java
@@ -1,13 +1,18 @@
 package com.yazilimxyz.enterprise_ticket_system;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 class EnterpriseTicketSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() { }
 }
