a323dbc81e9b67a8b6cb952b4bdc5c0c611ed6ef
Yeldadelidogan <yelda.ded@gmail.com>
2025-12-22 15:01:40 +0300

    Update TicketStatus.java

--- src/main/java/com/yazilimxyz/enterprise_ticket_system/entities/enums/TicketStatus.java
+++ src/main/java/com/yazilimxyz/enterprise_ticket_system/entities/enums/TicketStatus.java
@@ -1,11 +1,12 @@
 package com.yazilimxyz.enterprise_ticket_system.entities.enums;
 
+
 public enum TicketStatus {
     OPEN,
+    WAITING,
     IN_PROGRESS,
     RESOLVED,
-    CLOSED,
-    CANCELLED,
+    CLOSED
 }
 
 // TODO enumları fronttan alırken string şeklinde alıyoruz iyi mi kötü mü karar
