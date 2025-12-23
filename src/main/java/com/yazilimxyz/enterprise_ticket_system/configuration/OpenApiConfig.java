b631a04ec5b921aad4c1546e4af32c6abf974df1
Türker Kıvılcım <kivturker@gmail.com>
2025-12-23 23:18:30 +0300

    feat: add jwt field on swagger

---
+++ src/main/java/com/yazilimxyz/enterprise_ticket_system/configuration/OpenApiConfig.java
@@ -0,0 +1,37 @@
+package com.yazilimxyz.enterprise_ticket_system.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Enterprise Ticket System API")
+                        .version("1.0")
+                        .description("Ticket management system with authentication")
+                        .contact(new Contact()
+                                .name("Yazilim XYZ")
+                                .email("support@yazilimxyz.com")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT token giriniz (Bearer prefix olmadan)")));
+    }
+}
